fa3cce63db6a4c73e576d7dc33c50a0251316adf
Rushil Nagabhushan <33708124+rnagabhu@users.noreply.github.com>
2020-01-23 17:24:23 -0800

    Update A1.py

--- A1.py
+++ A1.py
@@ -1,17 +1,139 @@
-import sys
+
+
+import sys 
 import re
 import math
 from fractions import Fraction
 
-# Open the file in read mode
-#text = open(sys.argv[1], "r")
+# Open the file in read mode 
+#text = open(sys.argv[1], "r") 
 
 train = "1b_benchmark.train.tokens"
 dev = "1b_benchmark.dev.tokens"
 test = "1b_benchmark.test.tokens"
 
+preprocessedTrain = "train.preprocessed"
+preprocessedDev = "dev.preprocessed"
+preprocessedTest = "test.preprocessed"
+
 dashLine = "------------------------------------------------------------------------"
 
+def preprocessData(trainPath, devPath, testPath):
+    trainFile = open(trainPath, "r")
+
+    freq_dict = dict()
+    corpusSize = 0
+
+    for line in trainFile:
+
+        line = '<START> ' + line + ' <STOP>'
+        tokens = line.split(" ")
+        corpusSize = len(tokens) - 1
+
+        for token in tokens:
+            token = token.strip()
+
+            # if token is already in dictionary then increment the count
+            if token in freq_dict:
+                freq_dict[token] = freq_dict[token] + 1
+            # else initialize that token's key with value 1
+            elif token != '<START>':
+                freq_dict[token] = 1
+
+    vocabulary = {k:v for (k,v) in freq_dict.items() if v >= 3}
+    vocabulary['<UNK>'] = 0
+
+    unkwords = set()
+
+    for k, v in freq_dict.items():
+        if(v < 3): 
+            vocabulary['<UNK>'] = vocabulary['<UNK>'] + freq_dict[k]
+            unkwords.add(k)
+
+    ppTrain = open(preprocessedTrain, "w")
+    trainFile.close()
+    trainFile = open(trainPath, "r")
+
+    for line in trainFile:
+        newLine = line.strip().split()
+
+        tokens = line.strip().split(" ")
+
+        i = 0
+
+        while i < len(tokens):
+            tokens[i] = tokens[i].strip()
+            if tokens[i] in unkwords:
+                newLine[i] = '<UNK>'
+
+            i += 1
+
+        for token in newLine:
+            ppTrain.write(token + " ")
+
+        ppTrain.write("\n")
+
+    ppTrain.close()
+    trainFile.close()
+
+
+
+    devFile = open(devPath, "r")
+    ppDev = open(preprocessedDev, "w")
+
+    for line in devFile:
+        newLine = line.strip().split()
+
+        tokens = line.strip().split(" ")
+
+        i = 0
+
+        while i < len(tokens):
+            tokens[i] = tokens[i].strip()
+            if vocabulary.get(tokens[i], 0) == 0:
+                newLine[i] = '<UNK>'
+
+            i += 1
+
+        for token in newLine:
+            ppDev.write(token + " ")
+
+        ppDev.write("\n")
+
+    devFile.close()
+    ppDev.close()
+
+
+    testFile = open(testPath, "r")
+    ppTest = open(preprocessedTest, "w")
+
+    for line in testFile:
+        newLine = line.strip().split()
+
+        tokens = line.strip().split(" ")
+
+        i = 0
+
+        while i < len(tokens):
+            tokens[i] = tokens[i].strip()
+            if vocabulary.get(tokens[i], 0) == 0:
+                newLine[i] = '<UNK>'
+
+            i += 1
+
+        for token in newLine:
+            ppTest.write(token + " ")
+
+        ppTest.write("\n")
+
+    testFile.close()
+    ppTest.close()
+
+
+    return (vocabulary, corpusSize)
+
+# global variable for vocabulary and corpusSize
+(vocab, cS) = preprocessData('1b_benchmark.train.tokens','1b_benchmark.dev.tokens','1b_benchmark.test.tokens')
 
 class UnigramModel:
     def __init__(self, text):
@@ -19,7 +141,7 @@ class UnigramModel:
         textFile = open(text, "r")
         self.freq_dict = dict()
         self.corpusSize = 0
-        self.unkList = list()
+
         for line in textFile:
             # append stop token to EOLs
             line = line + ' <STOP>'
@@ -43,66 +165,64 @@ class UnigramModel:
 
     # this function replaces all keys with < 3 frequency with 'unk'
     def replaceUNKs(self, idict):
-        fdict = {k: v for (k, v) in idict.items() if v >= 3}
+        fdict = {k:v for (k,v) in idict.items() if v >= 3}
         fdict['unk'] = 0
         for k, v in idict.items():
-            if(v < 3):
-                self.unkList.append(k)
-                fdict['unk'] = fdict['unk'] + idict[k]
+            if(v < 3): fdict['<UNK>'] = fdict['<UNK>'] + idict[k]
         return fdict
 
     def calcTokenProb(self, token):
-        # The numerator is the frequency of the specified token, return 0 if not found in dict
+        #The numerator is the frequency of the specified token, return 0 if not found in dict
         unigram_numerator = self.freq_dict.get(token, 0)
 
-        # If frequency is 0, the word is unkown
+        #If frequency is 0, the word is unkown
         if unigram_numerator == 0:
-            unigram_numerator = self.freq_dict.get('unk', 0)
+            unigram_numerator = self.freq_dict.get('<UNK>', 0)
 
-        # The denominator is just the size of the corpus
+        #The denominator is just the size of the corpus
         unigram_denominator = self.corpusSize
         #print("Pr(" + token + "):")
         #print("numerator = ", unigram_numerator, "denominator = ", unigram_denominator)
         prob = Fraction(unigram_numerator, unigram_denominator)
+
         return prob
 
     def calcSentenceProb(self, sentence):
 
-        # Return 0 for empty sentence
+        #Return 0 for empty sentence
         if not sentence:
             return 0
 
         prob = Fraction(1)
-        # The MLE for a sentence is the product of the MLEs of its constituent tokens
+        #The MLE for a sentence is the product of the MLEs of its constituent tokens
         for token in sentence:
             prob = prob * self.calcTokenProb(token)
 
         return prob
 
-    # Takes a list of sentences and calculates the perplexity of the model given those senetences
+    #Takes a list of sentences and calculates the perplexity of the model given those senetences
     def calcPerplexityOLD(self, sentences):
         logSum = 0
 
-        # Keep track of how many tokens are in the sample we are testing
+        #Keep track of how many tokens are in the sample we are testing
         sampleSize = 0
 
-        # Find the log probability of each sentence and sum them all up
+        #Find the log probability of each sentence and sum them all up
         for sentence in sentences:
             sampleSize += len(sentence)
             sentenceProb = self.calcSentenceProb(sentence)
 
-            # Log is undefined if input is not > 0
+            #Log is undefined if input is not > 0
             if sentenceProb.numerator > 0:
-                logProb = math.log(sentenceProb.numerator, 2) - \
-                    math.log(sentenceProb.denominator, 2)
+                logProb = math.log(sentenceProb.numerator, 2) - math.log(sentenceProb.denominator, 2)
                 logSum += logProb
-            # Here we assume Pr = 0, so perplexity is infinite, return -1 to signify this
+            #Here we assume Pr = 0, so perplexity is infinite, return -1 to signify this
             else:
-                print("Sentence:" + str(sentence))
-                return -1
+               print("Sentence:" + str(sentence))
+               return -1
 
-        # To get perplexity, multiply this sum by the negative reciprocal
-        # of sample size and exponentiate it base 2
+        #To get perplexity, multiply this sum by the negative reciprocal 
+        #of sample size and exponentiate it base 2
         logSum = logSum * (-1 / float(sampleSize))
         perplexity = 2 ** logSum
 
@@ -111,38 +231,37 @@ class UnigramModel:
     def calcPerplexity(self, sentences):
         sampleLogSum = 0
 
-        # Keep track of how many tokens are in the sample we are testing
+        #Keep track of how many tokens are in the sample we are testing
         sampleSize = 0
 
-        # Find the log probability of each sentence and sum them all up
+        #Find the log probability of each sentence and sum them all up
         for sentence in sentences:
             sampleSize += len(sentence)
 
             sentenceLogSum = 0
 
-            # The log probability of each sentence is the sum of the log probabilities
-            # of its constituent tokens
+            #The log probability of each sentence is the sum of the log probabilities
+            #of its constituent tokens
             for token in sentence:
                 tokenProb = self.calcTokenProb(token)
 
                 if tokenProb > 0:
-                    sentenceLogSum += math.log(tokenProb.numerator, 2) - \
-                        math.log(tokenProb.denominator, 2)
+                    sentenceLogSum += math.log(tokenProb.numerator, 2) - math.log(tokenProb.denominator, 2)
                 else:
                     print("Token: " + token)
                     return -1
 
             sampleLogSum += sentenceLogSum
 
-        # To get perplexity, multiply this sum by the negative reciprocal
-        # of sample size and exponentiate it base 2
+        #To get perplexity, multiply this sum by the negative reciprocal 
+        #of sample size and exponentiate it base 2
         sampleLogSum = sampleLogSum * (-1 / float(sampleSize))
         perplexity = 2 ** sampleLogSum
 
         return perplexity
 
-    # Uses a test file and calculates perplexity based on that sample
 
+    #Uses a test file and calculates perplexity based on that sample
     def testModel(self, test):
         testFile = open(test, "r")
         print("Calculating perplexity of " + test)
@@ -162,11 +281,9 @@ class UnigramModel:
 
         return self.calcPerplexity(sentences)
 
-
-
 class BigramModel:
-    def __init__(self, text, unigramModel):
-        self.uni = unigramModel
+    def __init__(self, text):
+    #    self.uni = unigramModel
         textFile = open(text, "r")
         self.freq_dict = dict()
         self.corpusSize = 0
@@ -182,7 +299,7 @@ class BigramModel:
                  # else initialize that token's key with value 1
                  else:
                      self.freq_dict[bigram] = 1
-        self.replaceUNKs(self.freq_dict, self.uni.unkList)
+      #  self.replaceUNKs(self.freq_dict, self.uni.unkList)
 
     # BigramModel('1b_benchmark.test.tokens', UnigramModel('1b_benchmark.test.tokens'))
 
@@ -228,39 +345,39 @@ class BigramModel:
         bigramList = []
 
         while i < len(sentence):
-            bigramList.append((sentence[i - 1].strip(), sentence[i].strip()))
-            i += 1
+            bigramList.append((sentence[i - 1], sentence[i]))
+            i = i + 1
 
         return bigramList
 
-# textFile = open(text, "r")
-#         self.freq_dict = dict()
-#         self.corpusSize = 0
-#         for line in textFile:
-#             # append stop token to EOLs
-#             line = line + ' <STOP>'
-#             # split the line into tokens
-#             tokens = line.split(" ")
-#             # iterate through each token
-#             for token in tokens:
-#                 self.corpusSize += 1
-#                 # remove whitespace and \n characters
-#                 token = token.strip()
-#                 # if token is already in dictionary then increment the count
-#                 if token in self.freq_dict:
-#                     self.freq_dict[token] = self.freq_dict[token] + 1
-#                 # else initialize that token's key with value 1
-#                 else:
-#                     self.freq_dict[token] = 1
+    def calcTokenProb(self, tt):
+        bigram_numerator = self.freq_dict.get((tt[0],tt[1]), 0)
+        bigram_denominator = vocab.get(tt[0], 0)
+        if bigram_numerator == 0 or bigram_denominator == 0:
+            return 0
+        else: return Fraction(bigram_numerator, bigram_denominator)
+
+    def calcSentenceProb(self, sentence):
+        if not sentence: return 0
+
+        prob = Fraction(1)
+
+        for i in range(1, len(sentence) - 1):
+            prob = prob * self.calcTokenProb((sentence[i-1], sentence[i]))
+
+        return prob
 
 def main():
 
     path = sys.argv[1] + "/"
 
+    preprocessData(path + train, path + dev, path + test)
+
+    """
     print("\n" + dashLine)
     uni = UnigramModel(path + train)
     print(dashLine + "\n")
-
+    
     print("\n" + dashLine)
     print("Unigram train perplexity: " + str(uni.testModel(path + train)))
     print("")
@@ -268,6 +385,7 @@ def main():
     print("")
     print("Unigram test perplexity: " + str(uni.testModel(path + test)))
     print(dashLine + "\n")
+    """
 
 
 if __name__ == '__main__':
